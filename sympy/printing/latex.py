46f1dafed1450fccc4d6d3fdd979cd3b0612de47
MD-Levitan <ovsyanka@protonmail.com>
2026-08-28 15:02:09 +0200

    improve sequnce

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -605,7 +605,7 @@ class LatexPrinter(Printer):
             # Check if expr=log(x)/log(y) and transform to log_y(x)
             if (numer.is_Function and numer.func.__name__ == 'log' and len(numer.args) == 1) and\
                   (denom.is_Function and denom.func.__name__ == 'log' and len(denom.args) == 1):
-                tex += r"\log_{%s}{%s}" % \
+                tex = r"\log_{%s}{%s}" % \
                     (self._print(denom.args[0]), self._add_parens(self._print(numer.args[0])))
                 return tex
             snumer = convert(numer)
@@ -1101,7 +1101,7 @@ class LatexPrinter(Printer):
             if len(base) == 1:
                 tex = r"\log_%s{\left(%s \right)}" % (base, argument)
             else:
-                tex = r"\log11_{%s}{\left(%s \right)}" % (base, argument)
+                tex = r"\log_{%s}{\left(%s \right)}" % (base, argument)
         elif not self._settings["ln_notation"]:
             tex = r"\log{\left(%s \right)}" % self._print(expr.args[0])
         else:
